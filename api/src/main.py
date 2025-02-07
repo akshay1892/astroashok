19ef638df7be7968d1576e0751013f8cefd0a0ce
Akshay Mathur <akshaymathur1992@gmail.com>
2025-02-08 00:19:35 +0530

    API base code added

---
+++ api/src/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/")
+def index(request:Request):
+    return templates.TemplateResponse("index.html", request=request)
